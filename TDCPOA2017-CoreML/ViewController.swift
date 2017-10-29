4d36a9f132d1d56dcd5c06b84f0313f63379b884
Henrique Valcanaia <henrique_indalencio@hotmail.com>
2017-10-29 19:20:21 -0200

    Initial Commit

---
+++ TDCPOA2017-CoreML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TDCPOA2017-CoreML
+//
+//  Created by Henrique Valcanaia on 29/10/17.
+//  Copyright © 2017 Henrique Valcanaia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
